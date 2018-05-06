d3eb2cc1e0afa498cab9ec2e5babc88332bd5920
Sören Hentzschel <kontakt@agenedia.com>
2018-05-06 14:26:19 +0200

    order policy output alphabetically

--- src/js/core/policymanager.js
+++ src/js/core/policymanager.js
@@ -14,7 +14,33 @@ const policymanager = {
     policymanager.data.policies[key] = value;
   },
 
+  sort (object) {
+    if (object instanceof Array) {
+      const { length } = object;
+      for (let i = 0; i < length; i++) {
+        object[i] = policymanager.sort(object[i]);
+      }
+
+      return object;
+    }
+    else if (typeof object !== 'object') {
+      return object;
+    }
+
+    let keys = Object.keys(object);
+    keys = keys.sort();
+
+    const sorted = {};
+    const { length } = keys;
+
+    for (let i = 0; i < length; i++) {
+      sorted[keys[i]] = policymanager.sort(object[keys[i]]);
+    }
+
+    return sorted;
+  },
+
   getConfiguration () {
-    return JSON.stringify(policymanager.data, null, 2);
+    return JSON.stringify(policymanager.sort(policymanager.data), null, 2);
   }
 };
