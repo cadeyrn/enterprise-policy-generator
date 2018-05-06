58d889fa1693e5c6cfc4fa3df3429e1430240e88
Sören Hentzschel <kontakt@agenedia.com>
2018-05-06 14:47:50 +0200

    extracted output generation in own method

--- src/js/core/configurator.js
+++ src/js/core/configurator.js
@@ -106,117 +106,7 @@ const configurator = {
     elPolicyGeneratorButton.onclick = function (e) {
       e.preventDefault();
 
-      policymanager.init();
-
-      [...document.querySelectorAll('.primary-checkbox')].forEach((el) => {
-        if (el.checked) {
-          if (el.getAttribute('data-type') === 'array') {
-            const items = [];
-
-            [...el.parentNode.querySelectorAll(':scope > div')].forEach((el) => {
-              const item = { };
-
-              [...el.querySelectorAll(':scope input')].forEach((el) => {
-                if (el.value) {
-                  item[el.name] = el.value;
-                }
-              });
-
-              [...el.querySelectorAll(':scope select')].forEach((el) => {
-                let { value } = el.options[el.selectedIndex];
-
-                switch (value) {
-                  case 'true':
-                    value = true;
-                    break;
-                  case 'false':
-                    value = false;
-                    break;
-                  default:
-                  // do nothing
-                }
-
-                item[el.name] = value;
-              });
-
-              items.push(item);
-            });
-
-            if (items.length > 0) {
-              policymanager.add(el.name, items);
-            }
-          }
-          else if (el.getAttribute('data-type') === 'boolean') {
-            policymanager.add(el.name, !el.getAttribute('data-inverse'));
-          }
-          else if (el.getAttribute('data-type') === 'object') {
-            const policy = { };
-
-            [...el.parentNode.querySelectorAll(':scope > div > .array')].forEach((el) => {
-              const items = [];
-
-              [...el.querySelectorAll(':scope > .input input')].forEach((arrEl) => {
-                if (arrEl.value) {
-                  items.push(arrEl.value);
-                }
-              });
-
-              if (items.length > 0) {
-                policy[el.getAttribute('data-name')] = items;
-              }
-            });
-
-            [...el.parentNode.querySelectorAll(':scope > div > .checkbox input')].forEach((el) => {
-              if (el.checked) {
-                policy[el.name] = true;
-              }
-            });
-
-            [...el.parentNode.querySelectorAll(':scope > div > .enum select')].forEach((el) => {
-              let { value } = el.options[el.selectedIndex];
-
-              // null represents an empty state, there is nothing to do
-              if (value === 'null') {
-                return;
-              }
-
-              // if the value is a number treat it as number
-              if (!isNaN(value)) {
-                value = parseInt(value);
-              }
-
-              switch (value) {
-                case 'true':
-                  value = true;
-                  break;
-                case 'false':
-                  value = false;
-                  break;
-                default:
-                  // do nothing
-              }
-
-              policy[el.name] = value;
-            });
-
-            [...el.parentNode.querySelectorAll(':scope > div > .input input')].forEach((el) => {
-              if (el.value) {
-                policy[el.name] = el.value;
-              }
-            });
-
-            // only add non-empty policies
-            if (Object.keys(policy).length > 0) {
-              policymanager.add(el.name, policy);
-            }
-          }
-          else if (el.getAttribute('data-type') === 'string') {
-            policymanager.add(el.name, el.parentNode.querySelector('input[type=text]').value);
-          }
-        }
-      });
-
-      elPolicyOutput.innerText = policymanager.getConfiguration();
+      elPolicyOutput.innerText = configurator.generatePoliciesOutput();
     };
   },
 
@@ -586,6 +476,120 @@ const configurator = {
     }
 
     configurator.addOptionToUi(elObjectWrapper, policy.ui_category);
+  },
+
+  generatePoliciesOutput () {
+    policymanager.init();
+
+    [...document.querySelectorAll('.primary-checkbox')].forEach((el) => {
+      if (el.checked) {
+        if (el.getAttribute('data-type') === 'array') {
+          const items = [];
+
+          [...el.parentNode.querySelectorAll(':scope > div')].forEach((el) => {
+            const item = { };
+
+            [...el.querySelectorAll(':scope input')].forEach((el) => {
+              if (el.value) {
+                item[el.name] = el.value;
+              }
+            });
+
+            [...el.querySelectorAll(':scope select')].forEach((el) => {
+              let { value } = el.options[el.selectedIndex];
+
+              switch (value) {
+                case 'true':
+                  value = true;
+                  break;
+                case 'false':
+                  value = false;
+                  break;
+                default:
+                // do nothing
+              }
+
+              item[el.name] = value;
+            });
+
+            items.push(item);
+          });
+
+          if (items.length > 0) {
+            policymanager.add(el.name, items);
+          }
+        }
+        else if (el.getAttribute('data-type') === 'boolean') {
+          policymanager.add(el.name, !el.getAttribute('data-inverse'));
+        }
+        else if (el.getAttribute('data-type') === 'object') {
+          const policy = { };
+
+          [...el.parentNode.querySelectorAll(':scope > div > .array')].forEach((el) => {
+            const items = [];
+
+            [...el.querySelectorAll(':scope > .input input')].forEach((arrEl) => {
+              if (arrEl.value) {
+                items.push(arrEl.value);
+              }
+            });
+
+            if (items.length > 0) {
+              policy[el.getAttribute('data-name')] = items;
+            }
+          });
+
+          [...el.parentNode.querySelectorAll(':scope > div > .checkbox input')].forEach((el) => {
+            if (el.checked) {
+              policy[el.name] = true;
+            }
+          });
+
+          [...el.parentNode.querySelectorAll(':scope > div > .enum select')].forEach((el) => {
+            let { value } = el.options[el.selectedIndex];
+
+            // null represents an empty state, there is nothing to do
+            if (value === 'null') {
+              return;
+            }
+
+            // if the value is a number treat it as number
+            if (!isNaN(value)) {
+              value = parseInt(value);
+            }
+
+            switch (value) {
+              case 'true':
+                value = true;
+                break;
+              case 'false':
+                value = false;
+                break;
+              default:
+              // do nothing
+            }
+
+            policy[el.name] = value;
+          });
+
+          [...el.parentNode.querySelectorAll(':scope > div > .input input')].forEach((el) => {
+            if (el.value) {
+              policy[el.name] = el.value;
+            }
+          });
+
+          // only add non-empty policies
+          if (Object.keys(policy).length > 0) {
+            policymanager.add(el.name, policy);
+          }
+        }
+        else if (el.getAttribute('data-type') === 'string') {
+          policymanager.add(el.name, el.parentNode.querySelector('input[type=text]').value);
+        }
+      }
+    });
+
+    return policymanager.getConfiguration();
   }
 };
 
