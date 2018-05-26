0c80d44257e5bd70cd33c1921a4320c4fe7592f4
Sören Hentzschel <kontakt@agenedia.com>
2018-05-27 01:34:16 +0200

    added jsdoc for policymanager.js

--- src/js/core/policymanager.js
+++ src/js/core/policymanager.js
@@ -6,14 +6,34 @@
 const policymanager = {
   data : null,
 
+  /**
+   * This method creates an empty policies object.
+   *
+   * @returns {void}
+   */
   init () {
     policymanager.data = { policies : {} };
   },
 
+  /**
+   * This method adds a policy to the output object.
+   *
+   * @param {string} key - key of the policy
+   * @param {*} value - value of the policy, can be of any type
+   *
+   * @returns {void}
+   */
   add (key, value) {
     policymanager.data.policies[key] = value;
   },
 
+  /**
+   * This method sorts the output alphabetically.
+   *
+   * @param {Object} object - object to be sorted alphabetically
+   *
+   * @returns {Object} - the sorted object
+   */
   sort (object) {
     if (object instanceof Array) {
       const { length } = object;
@@ -40,6 +60,11 @@ const policymanager = {
     return sorted;
   },
 
+  /**
+   * Returns the JSON configuration as string. All policies are sorted alphabetically.
+   *
+   * @returns {string} - the JSON configuration as string
+   */
   getConfiguration () {
     return JSON.stringify(policymanager.sort(policymanager.data), null, 2);
   }
