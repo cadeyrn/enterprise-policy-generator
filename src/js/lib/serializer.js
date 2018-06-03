021462764f2f6cd1455049e96eedae1956c35f36
Sören Hentzschel <kontakt@agenedia.com>
2018-06-03 23:26:26 +0200

    added form serializer (not yet used in configurator)

---
+++ src/js/lib/serializer.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @exports serializer
+ */
+const serializer = {
+  serialize (form) {
+    const { elements } = form;
+    const { length } = elements;
+    const data = { };
+
+    data.checkboxes = { };
+    data.input = { };
+    data.select = { };
+
+    for (let i = 0; i < length; i++) {
+      const node = elements[i];
+
+      if (node.type === 'checkbox') {
+        if (node.checked) {
+          data.checkboxes[node.name] = node.value;
+        }
+      }
+      else if (node.type === 'text') {
+        if (node.value) {
+          data.input[node.name] = node.value;
+        }
+      }
+      else if (node.type === 'select-one') {
+        data.select[node.name] = node.value;
+      }
+    }
+
+    return data;
+  }
+};
