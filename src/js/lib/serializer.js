66e76fed8b2f4042df17da3a650f1c532376ef31
Sören Hentzschel <kontakt@agenedia.com>
2018-06-04 22:20:00 +0200

    changed order of if branches in serializer

--- src/js/lib/serializer.js
+++ src/js/lib/serializer.js
@@ -16,18 +16,21 @@ const serializer = {
     for (let i = 0; i < length; i++) {
       const node = elements[i];
 
+      // checkboxes
       if (node.type === 'checkbox') {
         if (node.checked) {
           data.checkboxes[node.name] = node.value;
         }
       }
+      // select fields
+      else if (node.type === 'select-one') {
+        data.select[node.name] = node.value;
+      }
+      // text fields
       else if (node.type === 'text') {
         if (node.value) {
           data.input[node.name] = node.value;
         }
-      }
-      else if (node.type === 'select-one') {
-        data.select[node.name] = node.value;
       }
     }
 
