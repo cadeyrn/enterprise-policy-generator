ed291fe54cf8335225ac1ea33ead6622c2630fbc
Sören Hentzschel <kontakt@agenedia.com>
2018-06-21 07:26:33 +0200

    since we already directly access the DOM in serializer.js we can also directly access the form element and don't need to pass it as parameter

--- src/js/core/serializer.js
+++ src/js/core/serializer.js
@@ -6,8 +6,8 @@
  * @exports serializer
  */
 const serializer = {
-  serialize (form) {
-    const { elements } = form;
+  serialize () {
+    const { elements } = document.getElementById('generator-form');
     const { length } = elements;
     const data = { };
 
