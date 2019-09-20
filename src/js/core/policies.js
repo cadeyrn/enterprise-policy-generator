c9d3fa0f0afde47cacba4f25c5436de3b47da6c4
Sören Hentzschel <kontakt@agenedia.com>
2019-09-20 21:52:22 +0200

    new policy to set the download directory and lock it, see #84

--- src/js/core/policies.js
+++ src/js/core/policies.js
@@ -1814,6 +1814,18 @@ const policies = {
     label : browser.i18n.getMessage('policy_description_DefaultDownloadDirectory_Label')
   },
 
+  DownloadDirectory : {
+    description : browser.i18n.getMessage('policy_description_DownloadDirectory'),
+    enterprise_only : false,
+    first_available : { mainstream : '68.0', esr : '68.0' },
+    info_link : null,
+    is_lockable : false,
+    ui_category : 'others',
+    mandatory : true,
+    type : 'string',
+    label : browser.i18n.getMessage('policy_description_DownloadDirectory_Label')
+  },
+
   HardwareAcceleration : {
     description : browser.i18n.getMessage('policy_description_HardwareAcceleration'),
     enterprise_only : false,
