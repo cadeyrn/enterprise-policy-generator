715d06206c4531ade7de2432e57434a27b19c321
Sören Hentzschel <kontakt@agenedia.com>
2026-02-07 19:14:42 +0100

    removed jsdoc, jsdoc-strip-async-await, and gulp-jsdoc3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,7 @@
 - Updated eslint-plugin-jsdoc from version 61.4.1 to 62.5.3
 - Updated stylelint from version 17.0.0 to 17.1.1
 - Replaced htmllint and gulp-htmllint with html-eslint
+- Removed jsdoc, jsdoc-strip-async-await, and gulp-jsdoc3
 
 ---
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 const gulp = require('gulp');
 const gulpEslint = require('gulp-eslint-new');
-const jsdoc = require('gulp-jsdoc3');
 
 gulp.task('lint-html', () => gulp.src(['./src/html/**/*.html'])
   .pipe(gulpEslint())
@@ -12,9 +11,4 @@ gulp.task('lint-html', () => gulp.src(['./src/html/**/*.html'])
 gulp.task('lint-js', () => gulp.src(['gulpfile.js', './src/js/**/*.js', './src/_locales/**/*.json'])
   .pipe(gulpEslint())
   .pipe(gulpEslint.format())
-);
-
-const jsdocsConfig = require('./jsdoc.json');
-gulp.task('docs', () => gulp.src(['CHANGELOG.md', 'README.md', './src/js/*/*.js'], { read : false })
-  .pipe(jsdoc(jsdocsConfig))
 );
